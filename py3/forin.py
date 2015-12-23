763172a66b332ab67b9876ec7af99e30d02850d2
root <72721755@qq.com>
2015-12-23 08:40:06 +0800

    py3

---
+++ py3/forin.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env  python3
+
+# -*- coding: utf-8 -*-
+
+names = ['a','b','c'];
+
+for i in names:
+    print(i)
+
+
+print(list(range(5)));
+
+
+sum=0
+for i in range(1, 101):
+    sum+=i;
+
+print(sum);
+
+sum=0;
+n=99;
+while n>0:
+    sum+=n;
+    n=n-1
+
+print(sum)
+
