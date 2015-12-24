13bb79ee2640015d1135e7e28d7fb41d81b0446a
persist <727271755@qq.com>
2015-12-24 10:56:41 +0800

    class animal

--- py3/module/class/student.py
+++ py3/module/class/student.py
@@ -11,15 +11,15 @@ __mtime__ = '2015/12/24'
 
 class Student(object):
     def __init__(self, name, score):
-        self.name = name
-        self.score = score
+        self.__name = name
+        self.__score = score
 
     def print_score(self):
-        print('%s:%s' % (self.name, self.score))
+        print('%s:%s' % (self.__name, self.__score))
 
 
 print(Student)
 
-bart=Student('zhangzw', 100)
-print(bart)
+bart = Student('zhangzw', 100)
+bart.__name = 'persist'
 bart.print_score()
