ce77a8cb78e93e60f7d026736693792d6af9df5b
zzw <zzw@163.com>
2015-12-27 15:43:14 +0800

    downloader

--- baike_spider/html_downloader.py
+++ baike_spider/html_downloader.py
@@ -7,8 +7,16 @@ __mtime__ = '2015/12/27'
 # code is far away from bugs with the god animal protecting
     I love animals. They taste delicious.
 """
+import urllib2
 
 
 class HtmlDownloader(object):
-    def download(self, new_url):
-        pass
+    def download(self, url):
+        if url == None:
+            return None
+
+        response = urllib2.urlopen(url)
+        if response.getcode() != 200:
+            return None
+
+        return response.read()
