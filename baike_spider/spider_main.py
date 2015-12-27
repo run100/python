41d1ec169f17d0a36169bfa375689859557e73df
zzw <zzw@163.com>
2015-12-27 15:27:45 +0800

    url manage

--- baike_spider/spider_main.py
+++ baike_spider/spider_main.py
@@ -20,13 +20,19 @@ class SpiderMain(object):
         self.outputer = html_outputer.HtmlOutputer()
 
     def craw(self, root_url):
+        count = 1
         self.urls.add_new_url(root_url)
         while self.urls.has_new_url():
             new_url = self.urls.get_new_url()
+            print("craw %d:%s" % (count, new_url))
             html_cont = self.downloader.download(new_url)
             new_urls, new_data = self.parser.parse(new_url, html_cont)
             self.urls.add_new_urls(new_urls)
             self.outputer.collect_data(new_data)
+
+            if ( count > 1000 ) :
+                break
+            count = count+1
 
         self.outputer.output_html()
 
