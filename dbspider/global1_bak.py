d7584a62faffb00aadc522208cd27f99afb3569f
persist8 <727271755@qq.com>
2016-01-13 14:41:09 +0800

    gloabl

---
+++ dbspider/global1_bak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'Administrator'
+__mtime__ = '2015/12/29'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+"""
+
+REDIS_HOST = "localhost"
+REDIS_PORT = "6379"
+REDIS_DB = 0
+
+MYSQL_HOST = "127.0.0.1"
+MYSQL_USER = "root"
+MYSQL_PASSWD = ""
+MYSQL_DB = "douban"
+MYSQL_CHARSET = "utf8"
