ba3dca882c4e84daa0d68a930f270c5314b60cf0
persist8 <727271755@qq.com>
2016-03-03 10:46:27 +0800

    r3

--- redis1/r3.py
+++ redis1/r3.py
@@ -12,10 +12,35 @@ import urllib2
 from bs4 import BeautifulSoup
 
 
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+}
+
 url = 'http://news.cnblogs.com/'
-resp = urllib2.urlopen(url)
+# resp = urllib2.urlopen(url)
 
-print(resp.getcode())
+request = urllib2.Request(url, headers=header)
 
+resp = urllib2.urlopen(request)
 
+#print(resp.getcode())
+if resp.getcode() == 200:
+    html_doc = resp.read()
+
+    soup = BeautifulSoup(html_doc, 'html.parser', from_encoding='utf-8')
+
+    news = soup.find_all('div', class_="news_block")
+
+    for new in news:
+        #print(new)
+        title = new.h2.a.string
+        print(title)
+        href = new.h2.a
+        print(href['href'])
+        #desc = new.select('div .entry_summary')
+        desc = new.find('div', class_="entry_summary")
+        print(desc.get_text())
+
+else:
+    print('error read page')
 
